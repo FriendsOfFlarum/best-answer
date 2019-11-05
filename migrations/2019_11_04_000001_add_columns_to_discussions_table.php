d118b4f6834a6ad0efb112e639998e097675805d
David Sevilla Martin <me@datitisev.me>
2019-11-05 13:25:05 -0500

    Allow migrating from older versions of the best answer ext

--- migrations/2019_11_04_000001_add_columns_to_discussions_table.php
+++ migrations/2019_11_04_000001_add_columns_to_discussions_table.php
@@ -6,17 +6,20 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        if ($schema->hasColumn('discussions', 'best_answer_notified')) {
-            return;
+        if (!$schema->hasColumn('discussions', 'best_answer_user_id')) {
+            $schema->table('discussions', function (Blueprint $table) {
+                $table->unsignedInteger('best_answer_post_id')->nullable();
+                $table->unsignedInteger('best_answer_user_id')->nullable();
+            });
         }
 
-        $schema->table('discussions', function (Blueprint $table) {
-            $table->unsignedInteger('best_answer_post_id')->nullable();
-            $table->unsignedInteger('best_answer_user_id')->nullable();
-            $table->boolean('best_answer_notified');
-        });
+        if (!$schema->hasColumn('discussions', 'best_answer_notified')) {
+            $schema->table('discussions', function (Blueprint $table) {
+                $table->boolean('best_answer_notified');
+            });
 
-        Discussion::query()->where('best_answer_notified', false)->update(['best_answer_notified' => 1]);
+            Discussion::query()->where('best_answer_notified', false)->update(['best_answer_notified' => 1]);
+        };
     },
     'down' => function (Builder $schema) {
         $schema->table('discussions', function (Blueprint $table) {
