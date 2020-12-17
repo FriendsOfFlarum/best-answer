29e9e3686ca443756bbecf426ea2a34e1b0de149
Ian Morland <imorland@users.noreply.github.com>
2020-12-17 04:28:23 +0000

    Apply fixes from StyleCI

--- extend.php
+++ extend.php
@@ -41,7 +41,7 @@ return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/resources/less/admin.less'),
-        
+
     new Extend\Locales(__DIR__.'/resources/locale'),
 
     new EnableConsole(),
