68703ef84ae519658a4d2c04b657ea07a73495ab
Ian Morland <ianm@giffgaff.co.uk>
2021-01-11 13:23:03 +0000

    COM-527 Use gambit negate, rather than two seperate gambits

--- src/Gambit/IsSolvedGambit.php
+++ src/Gambit/IsSolvedGambit.php
@@ -2,10 +2,8 @@
 
 namespace FoF\BestAnswer\Gambit;
 
-use Flarum\Discussion\Search\DiscussionSearch;
 use Flarum\Search\AbstractRegexGambit;
 use Flarum\Search\AbstractSearch;
-use LogicException;
 
 class IsSolvedGambit extends AbstractRegexGambit
 {
@@ -13,12 +11,12 @@ class IsSolvedGambit extends AbstractRegexGambit
 
     protected function conditions(AbstractSearch $search, array $matches, $negate)
     {
-        if (! $search instanceof DiscussionSearch) {
-            throw new LogicException('This gambit can only be applied on a DiscussionSearch');
-        }
-
-        $search->getQuery()->where(function ($query) {
-            $query->whereNotNull('best_answer_post_id');
+        $search->getQuery()->where(function ($query) use ($negate) {
+            if ($negate) {
+                $query->whereNull('best_answer_post_id');
+            } else {
+                $query->whereNotNull('best_answer_post_id');
+            }
         });
     }
 }
--- src/Listeners/AddGambits.php
+++ src/Listeners/AddGambits.php
@@ -4,13 +4,11 @@ namespace FoF\BestAnswer\Listeners;
 
 use Flarum\Event\ConfigureDiscussionGambits;
 use FoF\BestAnswer\Gambit\IsSolvedGambit;
-use FoF\BestAnswer\Gambit\IsUnsolvedGambit;
 
 class AddGambits
 {
     public function handle(ConfigureDiscussionGambits $event)
     {
         $event->gambits->add(IsSolvedGambit::class);
-        $event->gambits->add(IsUnsolvedGambit::class);
     }
 }
