fcde601493b01251df763555d53d230c09004ecc
Ian Morland <ian@morland.me>
2021-09-08 09:39:01 +0100

    Drop fof/components, use buildSettingComponent instead

--- js/src/admin/components/BestAnswerSettings.js
+++ js/src/admin/components/BestAnswerSettings.js
@@ -1,77 +1,89 @@
+import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
-import { settings } from '@fof-components';
-
-const {
-    items: { BooleanItem, SelectItem, StringItem, NumberItem },
-} = settings;
 
 export default class BestAnswerSettings extends ExtensionPage {
     oninit(vnode) {
         super.oninit(vnode);
-
-        this.setting = this.setting.bind(this);
     }
 
     content() {
         return [
             <div className="container">
                 <div className="BestAnswerSettingsPage">
-                    <div className="Introduction">
-                        <p>{app.translator.trans('fof-best-answer.admin.settings.introduction')}</p>
+                    <div className="Form">
+                        <div className="Introduction">
+                            <p>{app.translator.trans('fof-best-answer.admin.settings.introduction')}</p>
+                            <div className="Form-group">
+                                {this.buildSettingComponent({
+                                    type: 'boolean',
+                                    setting: 'fof-best-answer.enable_all_tags',
+                                    label: app.translator.trans('fof-best-answer.admin.settings.enable_all_tags'),
+                                    help: app.translator.trans('fof-best-answer.admin.settings.enable_all_tags_help'),
+                                })}
+                                {this.buildSettingComponent({
+                                    type: 'boolean',
+                                    setting: 'fof-best-answer.enable_all_tags_reminders',
+                                    label: app.translator.trans('fof-best-answer.admin.settings.enable_all_tags_reminders'),
+                                    help: app.translator.trans('fof-best-answer.admin.settings.enable_all_tags_reminders_help'),
+                                })}
+                            </div>
+                        </div>
+                        <div className="GeneralPreferences">
+                            <h3>{app.translator.trans('fof-best-answer.admin.settings.label.general')}</h3>
+                            {this.buildSettingComponent({
+                                type: 'boolean',
+                                setting: 'fof-best-answer.allow_select_own_post',
+                                label: app.translator.trans('fof-best-answer.admin.settings.allow_select_own_post'),
+                                help: app.translator.trans('fof-best-answer.admin.settings.allow_select_own_post_help'),
+                            })}
+                            {this.buildSettingComponent({
+                                type: 'boolean',
+                                setting: 'fof-best-answer.use_alternative_ui',
+                                label: app.translator.trans('fof-best-answer.admin.settings.use_alt_ui'),
+                                help: app.translator.trans('fof-best-answer.admin.settings.use_alt_ui_help'),
+                            })}
+                        </div>
+                        <hr />
+                        <div className="Reminders">
+                            <h3>{app.translator.trans('fof-best-answer.admin.settings.label.reminders')}</h3>
+                            <p className="helpText">
+                                {app.translator.trans('fof-best-answer.admin.settings.label.reminders_notice')}{' '}
+                                <a href="https://docs.flarum.org/console.html#schedule-run" target="_blank">
+                                    {app.translator.trans('fof-best-answer.admin.settings.documentation')}
+                                </a>
+                            </p>
+                            {this.buildSettingComponent({
+                                type: 'number',
+                                setting: 'fof-best-answer.select_best_answer_reminder_days',
+                                label: app.translator.trans('fof-best-answer.admin.settings.select_best_answer_reminder_days'),
+                                placeholder: '0',
+                                min: 0,
+                                help: app.translator.trans('fof-best-answer.admin.settings.select_best_answer_reminder_days_help'),
+                            })}
+                        </div>
+                        <hr />
+                        <div className="AdvancedPreferences">
+                            <h3>{app.translator.trans('fof-best-answer.admin.settings.label.advanced')}</h3>
+                            {this.buildSettingComponent({
+                                type: 'boolean',
+                                setting: 'fof-best-answer.schedule_on_one_server',
+                                label: app.translator.trans('fof-best-answer.admin.settings.schedule_on_one_server'),
+                                help: app.translator.trans('fof-best-answer.admin.settings.schedule_on_one_server_help'),
+                            })}
+                            {this.buildSettingComponent({
+                                type: 'boolean',
+                                setting: 'fof-best-answer.stop_overnight',
+                                label: app.translator.trans('fof-best-answer.admin.settings.schedule_stop_overnight'),
+                                help: app.translator.trans('fof-best-answer.admin.settings.schedule_stop_overnight_help'),
+                            })}
+                            {this.buildSettingComponent({
+                                type: 'boolean',
+                                setting: 'fof-best-answer.store_log_output',
+                                label: app.translator.trans('fof-best-answer.admin.settings.schedule_log_output'),
+                            })}
+                        </div>
+                        {this.submitButton()}
                     </div>
-                    <div className="GeneralPreferences">
-                        <h3>{app.translator.trans('fof-best-answer.admin.settings.label.general')}</h3>
-                        <div className="Form-group">
-                            <BooleanItem name="fof-best-answer.allow_select_own_post" setting={this.setting}>
-                                {app.translator.trans('fof-best-answer.admin.settings.allow_select_own_post')}
-                            </BooleanItem>
-                            <p className="helpText">{app.translator.trans('fof-best-answer.admin.settings.allow_select_own_post_help')}</p>
-                        </div>
-                        <div className="Form-group">
-                            <BooleanItem name="fof-best-answer.use_alternative_ui" setting={this.setting}>
-                                {app.translator.trans('fof-best-answer.admin.settings.use_alt_ui')}
-                            </BooleanItem>
-                            <p className="helpText">{app.translator.trans('fof-best-answer.admin.settings.use_alt_ui_help')}</p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="Reminders">
-                        <h3>{app.translator.trans('fof-best-answer.admin.settings.label.reminders')}</h3>
-                        <p className="helpText">
-                            {app.translator.trans('fof-best-answer.admin.settings.label.reminders_notice')}{' '}
-                            <a href="https://docs.flarum.org/console.html#schedule-run" target="_blank">
-                                {app.translator.trans('fof-best-answer.admin.settings.documentation')}
-                            </a>
-                        </p>
-                        <div className="Form-group">
-                            <NumberItem name="fof-best-answer.select_best_answer_reminder_days" placeholder="0" min="0" setting={this.setting}>
-                                {app.translator.trans('fof-best-answer.admin.settings.select_best_answer_reminder_days')}
-                            </NumberItem>
-                            <p className="helpText">{app.translator.trans('fof-best-answer.admin.settings.select_best_answer_reminder_days_help')}</p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="AdvancedPreferences">
-                        <h3>{app.translator.trans('fof-best-answer.admin.settings.label.advanced')}</h3>
-                        <div className="Form-group">
-                            <BooleanItem name="fof-best-answer.schedule_on_one_server" setting={this.setting}>
-                                {app.translator.trans('fof-best-answer.admin.settings.schedule_on_one_server')}
-                            </BooleanItem>
-                            <p className="helpText">{app.translator.trans('fof-best-answer.admin.settings.schedule_on_one_server_help')}</p>
-                        </div>
-                        <div className="Form-group">
-                            <BooleanItem name="fof-best-answer.stop_overnight" setting={this.setting}>
-                                {app.translator.trans('fof-best-answer.admin.settings.schedule_stop_overnight')}
-                            </BooleanItem>
-                            <p className="helpText">{app.translator.trans('fof-best-answer.admin.settings.schedule_stop_overnight_help')}</p>
-                        </div>
-                        <div className="Form-group">
-                            <BooleanItem name="fof-best-answer.store_log_output" setting={this.setting}>
-                                {app.translator.trans('fof-best-answer.admin.settings.schedule_log_output')}
-                            </BooleanItem>
-                        </div>
-                    </div>
-                    {this.submitButton()}
                 </div>
             </div>,
         ];
