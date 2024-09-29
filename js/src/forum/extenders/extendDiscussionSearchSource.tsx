b0f8c7d592a9436cb462f0c48fd14dc2ad36904f
IanM <ian@morland.me>
2024-09-29 10:03:21 +0100

    chore: add comment about 1.8.6 search source

--- js/src/forum/extenders/extendDiscussionSearchSource.tsx
+++ js/src/forum/extenders/extendDiscussionSearchSource.tsx
@@ -4,6 +4,7 @@ import DiscussionsSearchSource from 'flarum/forum/components/DiscussionsSearchSo
 
 export default function extendDiscussionsSearchSource() {
   extend(DiscussionsSearchSource.prototype, 'queryMutators', function (mutators: string[]) {
+    // This will only apply when using flarum/core 1.8.6 or newer
     if (app.forum.attribute<boolean>('solutionSearchEnabled')) {
       mutators.push('-is:solved');
     }
