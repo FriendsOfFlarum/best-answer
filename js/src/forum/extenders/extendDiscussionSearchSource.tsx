b3706dc2a0e6e8f8e0bfe1169aa35849c04136e7
IanM <ian@morland.me>
2024-11-01 20:15:53 +0000

    chore: remove unneeded tsignore

--- js/src/forum/extenders/extendDiscussionSearchSource.tsx
+++ js/src/forum/extenders/extendDiscussionSearchSource.tsx
@@ -3,7 +3,6 @@ import { extend } from 'flarum/common/extend';
 import DiscussionsSearchSource from 'flarum/forum/components/DiscussionsSearchSource';
 
 export default function extendDiscussionsSearchSource() {
-  @ts-ignore
   extend(DiscussionsSearchSource.prototype, 'queryMutators', function (mutators: string[]) {
     // This will only apply when using flarum/core 1.8.6 or newer
     if (app.forum.attribute<boolean>('removeSolutionResultsFromMainSearch')) {
