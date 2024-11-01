4ab4534aa8c5de42878f0d4cdfa09882429ef0fb
IanM <16573496+imorland@users.noreply.github.com>
2024-11-01 20:13:08 +0000

    Update js/src/forum/extenders/extendDiscussionSearchSource.tsx
    Co-authored-by: Davide Iadeluca <146922689+DavideIadeluca@users.noreply.github.com>

--- js/src/forum/extenders/extendDiscussionSearchSource.tsx
+++ js/src/forum/extenders/extendDiscussionSearchSource.tsx
@@ -3,7 +3,8 @@ import { extend } from 'flarum/common/extend';
 import DiscussionsSearchSource from 'flarum/forum/components/DiscussionsSearchSource';
 
 export default function extendDiscussionsSearchSource() {
-  extend(DiscussionsSearchSource.prototype, 'queryMutators', function (mutators) {
+  @ts-ignore
+  extend(DiscussionsSearchSource.prototype, 'queryMutators', function (mutators: string[]) {
     // This will only apply when using flarum/core 1.8.6 or newer
     if (app.forum.attribute<boolean>('removeSolutionResultsFromMainSearch')) {
       mutators.push('-is:solved');
